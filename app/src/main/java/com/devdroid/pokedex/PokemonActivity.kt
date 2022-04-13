3fc8febb47f81a58d98f55796d268e999bfc426f
João Pedro Mesquita <jp.devdroid@gmail.com>
2022-04-13 09:58:54 -0300

    Feat: Add back navigation from Pokemon Screen to Main Screen

--- app/src/main/java/com/devdroid/pokedex/PokemonActivity.kt
+++ app/src/main/java/com/devdroid/pokedex/PokemonActivity.kt
@@ -1,6 +1,7 @@
 package com.devdroid.pokedex
 
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -18,6 +19,9 @@ class PokemonActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pokemon)
+
+        val actionBar = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
 
         val id = intent.getStringExtra("url")
             ?.split("/")
@@ -57,5 +61,15 @@ class PokemonActivity : AppCompatActivity() {
                 }
             )
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
